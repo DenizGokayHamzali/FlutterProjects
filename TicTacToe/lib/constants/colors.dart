c2397f3844d30a58f77d39a5f1002908c89c33a3
DenizGokayHamzali <denizgokayhamzali@gmail.com>
2024-05-23 22:07:47 +0300

    TicTacToe added

---
+++ TicTacToe/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class MainColor {
+  static Color primaryColor = const Color.fromRGBO(0, 244, 255, 1);
+  static Color secondaryColor = const Color.fromRGBO(255, 160, 255, 1);
+  static Color accentColor = const Color.fromRGBO(255, 202, 39, 1);
+}
