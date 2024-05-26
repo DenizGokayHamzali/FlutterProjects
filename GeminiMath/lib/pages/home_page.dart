76864c1463482add9ff3eb8a769314d00de08098
DenizGokayHamzali <denizgokayhamzali@gmail.com>
2024-05-26 19:35:26 +0300

    GeminiMath 2/2 added

--- GeminiMath/lib/pages/home_page.dart
+++ GeminiMath/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
@@ -13,6 +15,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<StatefulWidget> {
   XFile? _image;
+  String _responseBody = "";
+  bool _isSending = false;
 
   Future<void> _getImageFromCamera() async {
     final ImagePicker picker = ImagePicker();
@@ -45,6 +49,76 @@ class _MyHomePageState extends State<StatefulWidget> {
     }
   }
 
+  Future<void> sendImage(XFile? imagefile) async {
+    if(imagefile == null) return;
+    setState(() {
+      _isSending = true;
+    });
+    String base64Image = base64Encode(File(imagefile.path).readAsBytesSync());
+    String apiKey = "AIzaSyBZOTEki_cqkcMTZBHnkt9RWlPshdt8HVc";
+    String requestBody = json.encode(
+      {
+        "contents": [
+          {
+            "parts": [
+              {
+                "text": "Solve this maths function and write step-by-step details and the reason behind that step\n"
+              },
+              {
+                "inlineData": {
+                  "mimeType": "image/jpeg",
+                  "data": base64Image
+                }
+              }
+            ]
+          }
+        ],
+        "generationConfig": {
+          "temperature": 0.4,
+          "topK": 32,
+          "topP": 1,
+          "maxOutputTokens": 4096,
+          "stopSequences": []
+        },
+        "safetySettings": [
+          {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+          {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+          {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+          {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+        ],
+      }
+    );
+    http.Response response = await http.post(
+      Uri.parse("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$apiKey"),
+      headers: {"Content-Type": "application/json"},
+      body: requestBody
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonBody = json.decode(response.body);
+      setState(() {
+        _responseBody = jsonBody["candidates"][0]["content"]["parts"][0]["text"];
+        _isSending = false;
+      });
+    } else {
+      setState(() {
+        _isSending = false;
+      });
+    }
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,12 +127,24 @@ class _MyHomePageState extends State<StatefulWidget> {
       ),
       body: Stack(
         children: [
-          _image == null ? const Text("No Image is selected!") : Image.file(File(_image!.path)),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _image == null ? const Text("No Image is selected!") : Image.file(File(_image!.path)),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(_responseBody, style: const TextStyle(fontSize: 16)),
+                )
+              ],
+            ),
+          ),
+          if(_isSending) const Center(child: CircularProgressIndicator(color: Colors.red),),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _openCamera();
+          _image == null ? _openCamera() : sendImage(_image);
         },
         tooltip: _image == null ? "Pick image" : "Send image",
         child: Icon(_image == null ? Icons.camera_alt : Icons.send),
